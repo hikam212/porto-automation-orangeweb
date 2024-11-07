0d11194ea18368e865d9a8dc5791ae1cfc17490a
Hikam Haudli <hikamhaudli@gmail.com>
2024-11-07 20:02:25 +0700

    [UPDATE] Login

--- tests/Porto-automation-orangeWeb/Login.spec.ts
+++ tests/Porto-automation-orangeWeb/Login.spec.ts
@@ -14,22 +14,3 @@ const isDashboardVisible = await page.getByRole('heading', { name: 'Dashboard' }
 console.log('Telah masuk ke halaman Dashboard', isDashboardVisible);
 
 });
-
-test('Search menu', async ({ page }) => {
-  async function searchAndOpenSubmenu(page, Admin, PIM) {
-    // Fill in the search input with the menu name
-    await page.getByPlaceholder('Search').fill(Admin);
-    
-    // Click on the main menu item (e.g., "Admin")
-    await page.getByRole('menuitem', { name: Admin }).click();
-  
-    await page.getByPlaceholder('Search').fill(PIM);
-
-    // Wait for the submenu to be visible and then click on it
-    await page.getByRole('menuitem', { name: PIM }).click();
-    
-    // Optionally, verify that the submenu page has loaded
-    const isSubmenuVisible = await page.getByText(PIM).isVisible();
-    expect(isSubmenuVisible).toBe(true);
-  }
-});
