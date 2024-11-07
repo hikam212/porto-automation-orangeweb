ac893b2e0ac19e569cfece479b00fc7c3b51cf44
Hikam Haudli <hikamhaudli@gmail.com>
2024-11-07 20:05:20 +0700

    [UPDATE] Login

--- tests/Porto-automation-orangeWeb/Login.spec.ts
+++ tests/Porto-automation-orangeWeb/Login.spec.ts
@@ -2,15 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-});
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  const isDashboardVisible = await page.getByRole('heading', { name: 'Dashboard' }).isVisible();
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-await page.getByPlaceholder('Username').fill('Admin');
-await page.getByPlaceholder('Password').fill('admin123');
-await page.getByRole('button', { name: 'Login' }).click();
-const isDashboardVisible = await page.getByRole('heading', { name: 'Dashboard' }).isVisible();
-
-console.log('Telah masuk ke halaman Dashboard', isDashboardVisible);
-
+  console.log('Telah masuk ke halaman Dashboard', isDashboardVisible);
 });
